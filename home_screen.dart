5d28ce4154e20afe90c492cc90e2d4d621c15123
gavinahoffman <hoff1710@d.umn.edu>
2026-01-18 19:26:14 -0600

    home_screen.dart

---
+++ home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue.shade50,
+      body: Center(
+        child: Text(
+          'Welcome to Homework Tracker!',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.blueAccent,
+          ),
+        ),
+      ),
+    );
+  }
+}
